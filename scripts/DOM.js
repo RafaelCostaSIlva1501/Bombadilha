cd0240890836952ead91f100216122f4ab09f4ba
Rafael da Costa Silva <rafael1501.oficial@gmail.com>
2025-04-23 16:50:58 -0300

    update

---
+++ scripts/DOM.js
@@ -0,0 +1,7 @@
+export const DOM = {
+  header: document.querySelector(".header"),
+  menu: document.querySelector(".menu"),
+  btnMenu: document.querySelectorAll(".btn-menu"),
+  icons: document.querySelector(".icons"),
+  banner: document.querySelector(".banner"),
+};
--- scripts/script.js
+++ scripts/script.js
@@ -1,197 +1,52 @@
-// Adiciona um event listener ao botão de menu, que alterna a classe "open" no elemento com o ID "menu" ao clicar
-document.getElementById("btnMenu").addEventListener("click", () => {
-    document.getElementById("menu").classList.toggle("open");
+import { DOM } from "./DOM.js";
+
+let menuON = false;
+
+DOM.btnMenu.forEach((e) => {
+  e.addEventListener("click", () => {
+    menuON = !menuON;
+
+    DOM.menu.style.width = menuON ? "220px" : "0px";
+    DOM.menu.style.padding = menuON ? "20px 20px" : " 20px 0px";
+  });
 });
 
-// Define o índice inicial do jogador a ser mostrado
-let playerIndex = 1;
+const createElement = (tag) => {
+  const element = document.createElement(tag);
+  return element;
+};
 
-// Seleciona todos os elementos com a classe "player"
-const player = document.querySelectorAll(".player");
-
-// Define um array de objetos que contém informações sobre os jogadores, como nome, idade, altura, posição e imagem
-const players = [
-    {
-        name: "",
-        age: "",
-        height: "",
-        position: "",
-        img: "bruno-lopes",
-    },
-    {
-        name: "",
-        age: "",
-        height: "",
-        position: "",
-        img: "caua-nunes",
-    },
-    {
-        name: "",
-        age: "",
-        height: "",
-        position: "",
-        img: "danilo-oliveira",
-    },
-    {
-        name: "",
-        age: "",
-        height: "",
-        position: "",
-        img: "davi-reis",
-    },
-    {
-        name: "",
-        age: "",
-        height: "",
-        position: "",
-        img: "felipe-bordignon",
-    },
-    {
-        name: "",
-        age: "",
-        height: "",
-        position: "",
-        img: "felipe-oliveira",
-    },
-    {
-        name: "",
-        age: "",
-        height: "",
-        position: "",
-        img: "guilherme-costa",
-    },
-    {
-        name: "",
-        age: "",
-        height: "",
-        position: "",
-        img: "guilherme-macedo",
-    },
-    {
-        name: "",
-        age: "",
-        height: "",
-        position: "",
-        img: "luan-lopes",
-    },
-    {
-        name: "",
-        age: "",
-        height: "",
-        position: "",
-        img: "manga",
-    },
-    {
-        name: "",
-        age: "",
-        height: "",
-        position: "",
-        img: "marcio-reis",
-    },
-    {
-        name: "",
-        age: "",
-        height: "",
-        position: "",
-        img: "marcos",
-    },
-    {
-        name: "",
-        age: "",
-        height: "",
-        position: "",
-        img: "pablo-almeida",
-    },
-    {
-        name: "",
-        age: "",
-        height: "",
-        position: "",
-        img: "reinan",
-    },
-    {
-        name: "",
-        age: "",
-        height: "",
-        position: "",
-        img: "remerson-coelho",
-    },
-    {
-        name: "",
-        age: "",
-        height: "",
-        position: "",
-        img: "thobias",
-    },
-    {
-        name: "",
-        age: "",
-        height: "",
-        position: "",
-        img: "tiozim",
-    },
-    {
-        name: "",
-        age: "",
-        height: "",
-        position: "",
-        img: "wendre-lima",
-    },
+const player = [
+  "bruno-lopes",
+  "caua-nunes",
+  "danilo-oliveira",
+  "davi-reis",
+  "felipe-bordignon",
+  "felipe-oliveira",
+  "guilherme-costa",
+  "guilherme-macedo",
+  "luan-lopes",
+  "manga",
+  "marcio-reis",
+  "marcos",
+  "pablo-almeida",
+  "reinan",
+  "remerson-coelho",
+  "thobias",
+  "tiozim",
+  "wendre-lima",
 ];
 
-// Função para exibir os jogadores na tela
-const showPlayers = () => {
-    // Define a imagem central como o jogador atual
-    player[1].src = `img/players/${players[playerIndex].img}.jpg`;
+player.map((e) => {
+  const icon = createElement("img");
+  icon.src = `../img/players/${e}-icon.jpg`;
 
-    // Define a imagem à esquerda como o próximo jogador
-    player[0].src = `img/players/${players[playerIndex + 1].img}.jpg`;
+  DOM.icons.appendChild(icon);
 
-    // Define a imagem à direita como o jogador anterior
-    player[2].src = `img/players/${players[playerIndex - 1].img}.jpg`;
-};
-
-// Função para mudar o jogador exibido ao clicar nos botões de navegação
-const changePlayer = (change) => {
-    if (change === "sub") {
-        // Se "sub" é passado, decrementa o índice do jogador
-        playerIndex = playerIndex - 1;
-
-        // Se o índice for menor que 0, volta para o último jogador do array
-        if (playerIndex < 0) {
-            playerIndex = players.length - 1;
-        }
-
-        // Adiciona a animação de deslizamento para a esquerda
-        player[2].classList.add("leftPlayer");
-
-        // Remove a animação após 0,5 segundos e mostra os novos jogadores
-        setTimeout(() => {
-            player[2].classList.remove("leftPlayer");
-            showPlayers();
-        }, 500);
-    } else if (change === "add") {
-        // Se "add" é passado, incrementa o índice do jogador
-        playerIndex = playerIndex + 1;
-
-        // Se o índice for maior que o tamanho do array, volta para o primeiro jogador
-        if (playerIndex > players.length - 1) {
-            playerIndex = 0;
-        }
-
-        // Adiciona a animação de deslizamento para a direita
-        player[0].classList.add("rightPlayer");
-
-        // Remove a animação após 0,5 segundos e mostra os novos jogadores
-        setTimeout(() => {
-            player[0].classList.remove("rightPlayer");
-            showPlayers();
-        }, 500);
-    }
-
-    // Loga o índice atual do jogador no console
-    console.log(playerIndex);
-};
-
-// Chama a função para mostrar os jogadores iniciais ao carregar a página
-showPlayers();
+  icon.addEventListener("click", () => {
+    DOM.banner.innerHTML = ""
+    const img = createElement("img");
+    img.src = `../img/players/${e}.jpg`;
+    DOM.banner.appendChild(img);
+  });
+});
